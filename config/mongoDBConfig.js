5fb4af67798c99dca32f3da567d6e1bdfb340240
Abhishek Kumar Rai <75179538+abhishek00210@users.noreply.github.com>
2024-11-20 17:52:06 +0530

    Update mongoDBConfig.js

--- config/mongoDBConfig.js
+++ config/mongoDBConfig.js
@@ -1,17 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
+      useNewUrlParser: true, // Ensures that the connection uses the new URL parser.
+      useUnifiedTopology: true, // Ensures that the connection uses the new topology engine.
     });
-
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
-    process.exit(1);
+    process.exit(1); // Exit the process with failure
   }
 };
 
